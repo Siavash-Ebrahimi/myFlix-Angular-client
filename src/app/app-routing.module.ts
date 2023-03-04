7e53de27815e68da2433eb60eb62bd861766a418
Siavash Ebrahimi <ebrahimi.siavash@gmail.com>
2023-03-04 11:30:20 +0400

    Added all app endpoints to fetch-api-dada.service.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+/* The HttpClientModule is a simplified API for Angular applications that 
+   makes it possible for the client app to communicate with the API or 
+   server-side. */
+import { HttpClientModule } from '@angular/common/http';
+
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
